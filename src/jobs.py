010381e54ad82759cbd19d65896c6ca11ec47cad
GustavoPD <gudalmoro@gmail.com>
2022-08-21 21:08:35 -0300

    Implementa jobs

--- src/jobs.py
+++ src/jobs.py
@@ -1,8 +1,13 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
 def read(path):
+    with open(path, encoding="utf-8") as csv_file:
+        readFile = csv.DictReader(csv_file, delimiter=",", quotechar='"')
+        return list(readFile)
+
     """Reads a file from a given path and returns its contents
 
     Parameters
