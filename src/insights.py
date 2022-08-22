004d5f45dd201e211e8a3d41f29dd14c7e38869a
GustavoPD <gudalmoro@gmail.com>
2022-08-21 21:52:35 -0300

    Implementa get min salary'

--- src/insights.py
+++ src/insights.py
@@ -65,21 +65,13 @@ def get_max_salary(path):
 
 
 def get_min_salary(path):
-    """Get the minimum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The minimum salary paid out of all job opportunities
-    """
-    pass
+    listSalary = []
+    list = read(path)
+    for obj in list:
+        if obj["min_salary"] != "invalid" and \
+           obj["min_salary"] != "":
+            listSalary.append(int(obj["min_salary"]))
+    return min(listSalary)
 
 
 def matches_salary_range(job, salary):
