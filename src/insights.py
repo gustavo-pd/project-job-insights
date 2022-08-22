f2fe3e61573c58c1f0a5065ed82dc5f513aba5f8
GustavoPD <gudalmoro@gmail.com>
2022-08-22 02:56:40 -0300

    Implementa o filtro de emprego pela industria

--- src/insights.py
+++ src/insights.py
@@ -27,21 +27,11 @@ def get_unique_industries(path):
 
 
 def filter_by_industry(jobs, industry):
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    return []
+    list = []
+    for obj in jobs:
+        if obj["industry"] == industry:
+            list.append(obj)
+    return list
 
 
 def get_max_salary(path):
