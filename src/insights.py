76514a0b768840ba38a7c19b7e68a05773eaa0ff
GustavoPD <gudalmoro@gmail.com>
2022-08-22 03:19:01 -0300

    Implementa matches salary range

--- src/insights.py
+++ src/insights.py
@@ -54,30 +54,23 @@ def get_min_salary(path):
     return min(listSalary)
 
 
+def verify_salary_error(min, max, value):
+    if type(min) != int or min == "":
+        raise ValueError
+    if type(max) != int or max == "":
+        raise ValueError
+    if max < min:
+        raise ValueError
+
+
 def matches_salary_range(job, salary):
-    """Checks if a given salary is in the salary range of a given job
-
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    pass
+    try:
+        max_salary = job["max_salary"]
+        min_salary = job["min_salary"]
+        verify_salary_error(min_salary, max_salary, salary)
+        return min_salary <= salary <= max_salary
+    except KeyError:
+        raise ValueError
 
 
 def filter_by_salary_range(jobs, salary):
